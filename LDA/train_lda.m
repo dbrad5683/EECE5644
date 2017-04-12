7082fddeec711ab091021d21f4177e4f1c71d269
CooperL <cooper.loughlin@gmail.com>
2017-04-12 17:12:49 -0400

    wrote function for LDA

---
+++ LDA/train_lda.m
@@ -0,0 +1,51 @@
+function w = train_lda(trainTDM_full,trainLabels,labels,dim)
+K = length(labels);
+%% de-mean everything
+trainMean = mean(trainTDM_full,2);
+trainTDM_dm = trainTDM_full - repmat(trainMean,1,numTrain);
+%% calculate SVD and transform data
+disp('SVD');
+[U,S,~] = svds(trainTDM_dm,dim);
+%% tranform data
+T = U*S;
+trainTDM = T'*trainTDM_dm;
+%% separate training data into and estimating parameters
+disp('estimating parameters');
+classTDM = cell(K,1);
+classMeans = zeros(dim,K);
+classCovs = zeros(dim,dim,K);
+classTotal = zeros(K,1);
+for ii=1:K
+    currMes = labels{ii};
+    idx = strcmpi(currMes,trainMes);
+    classTotal(ii) = sum(idx);
+    classTDM{ii} = trainTDM(:,idx);
+    % get parameters
+    classMeans(:,ii) = mean(classTDM{ii},2);
+    classCovs(:,:,ii) = cov(classTDM{ii}');
+    disp(['class num: ', num2str(ii)]);
+end
+classPrior = classTotal./numTrain;
+%% get background means and covariances
+disp('background parameters');
+backgroundMeans = zeros(dim,K);
+backgroundCovs = zeros(dim,dim,K);
+idx = 1:K;
+for ii=1:K
+    % make background TDM
+    currMes = labels{ii};
+    backgroundTDM = trainTDM(:,~strcmpi(currMes,trainMes));
+    backgroundMeans(:,ii) = mean(backgroundTDM,2);
+    backgroundCovs(:,:,ii) = cov(backgroundTDM');
+    disp(['class num: ', num2str(ii)]);
+end
+%% get w
+disp('calculating w');
+w = zeros(dim,K);
+for ii=1:K
+    w(:,ii) = eye(dim)\(backgroundCovs(:,:,ii)+classCovs(:,:,ii))*(classMeans(:,ii)-backgroundMeans(:,ii));
+    disp(['class num: ', num2str(ii)]);
+end
+
+end
+
